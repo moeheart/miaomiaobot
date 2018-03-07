0e09249c719b93b50472b51a60f28e0c108fb554
moeheart <957685908@qq.com>
2018-03-07 15:01:58 +0800

    add post

--- base.pl
+++ base.pl
@@ -4,9 +4,9 @@
  
  $host = "0.0.0.0"; #发送消息接口监听地址，没有特殊需要请不要修改
  $port = 5000;      #发送消息接口监听端口，修改为自己希望监听的端口
- #$post_api = 'http://xxxx';  #接收到的消息上报接口，如果不需要接收消息上报，可以删除或注释此行
+ $post_api = 'http://localhost:8888';  #接收到的消息上报接口，如果不需要接收消息上报，可以删除或注释此行
  
  my $client = Mojo::Webqq->new();
  $client->load("ShowMsg");
- $client->load("Openqq",data=>{listen=>[{host=>$host,port=>$port}], post_api=>$post_api});
+ $client->load("Openqq",data=>{listen=>[{host=>$host,port=>$port}], post_api=>$post_api, post_event => 0, post_stdout => 0});
  $client->run();
