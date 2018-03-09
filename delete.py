6e2f836692a89d8b1374d7847b3534437dcfa6d6
moeheart <957685908@qq.com>
2018-03-09 12:55:00 +0800

    delete

---
+++ delete.py
@@ -0,0 +1,15 @@
+import pymysql
+
+db = pymysql.connect("172.21.0.10","root","testpwd1","test",port=5000,charset='utf8')
+
+cursor = db.cursor()
+
+sql = """DELETE FROM schedule WHERE sch = '周五'"""
+cursor.execute(sql)
+
+sql = """DELETE FROM playerinfo WHERE sch = '周五'"""
+cursor.execute(sql)
+
+        
+db.commit()
+db.close()
