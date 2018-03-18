5476c0f42e6a46e25b327c9967a1f2f1e22ce0b9
moeheart <957685908@qq.com>
2018-03-18 20:34:24 +0800

    2.5 fb 3

--- base.pl
+++ base.pl
@@ -6,7 +6,7 @@
  $port = 5000;      #发送消息接口监听端口，修改为自己希望监听的端口
  $post_api = 'http://localhost:8888';  #接收到的消息上报接口，如果不需要接收消息上报，可以删除或注释此行
  $poll_api = 'http://localhost:8888/testalive';
- $poll_interval = 600;
+ $poll_interval = 1800;
  
  my $client = Mojo::Webqq->new();
  $client->load("ShowMsg");
